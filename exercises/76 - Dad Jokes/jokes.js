5f0169c977f67f27167d337582fed6e162d2a22a
Cameron <56660090+kaminji-c@users.noreply.github.com>
2023-10-10 13:54:12 -0700

    76 complete

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -1,3 +1,9 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeButtonSpan = jokeButton.querySelector('.jokeText');
+const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
+
+
 const buttonText = [
   'Ugh.',
   '🤦🏻‍♂️',
@@ -8,3 +14,54 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+
+async function fetchJoke(){
+  //turn  loader on when we fetch something
+  loader.classList.remove('hidden');
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept:'application/json',
+    },
+  });
+  const data = await response.json();
+  //similarly you can just return it this way, wes prefers to do it the first way, to use console.log() easily 
+  //return response.json();
+  //turn loader off when data returns
+  loader.classList.add('hidden');
+  return data;
+}
+
+//function changes joke button text content
+//function uses the arr to pass in arr we want because we want this to be a utility function and not specific to a certain array, allows us to use it for whatever array we want!!!
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+  //where we pass in not, sometimes when we generate random it populates the same result
+  //when a function calls itself, RECURSION...if the thing is the same the same time, then run it again
+  if(item === not) {
+    console.log('Ahh we used that one last tie, look again');
+    console.log(not);
+    //in console run 'randomItemFromArray(buttonText, "stop it.")' to see the console log, utilize not
+    return randomItemFromArray(arr, not);
+  }
+  return item;
+}
+//randomItemFromArray(buttonText) run in console.log SOOOO COOOL !!!
+
+
+
+
+//function generates joke and populates it
+async function handleClick(){ 
+  //destructuring, because we only want the joke data in a variable called joke
+  const { joke } = await fetchJoke();
+  //console.log(joke);
+  jokeHolder.textContent = joke;
+  jokeButtonSpan.textContent = randomItemFromArray(buttonText, jokeButtonSpan.textContent); //second arg, whatever the button says don't return that one to me because thats already the current one
+}
+
+jokeButton.addEventListener('click', handleClick);
+
+
+//What is a header, a header is some additional information that comes along with a request, can add additional info to the api, by adding an Object inside another Object
+
+//Console > Network Tab > API you are trying to request from > Headers 
--- exercises/77 - Currency Converter/money.js
+++ exercises/77 - Currency Converter/money.js
@@ -1,3 +1,8 @@
+//this lesson includes fetching data, caching data
+//because when we hit this API we need to provide it our base rate
+const fromSelect = document.querySelector('[name="from_currency"]');
+const toSelect = document.querySelector('[name="to_currency"]');
+const endpoint = 'https//api.exchangeratesapi.io/latest';
 const currencies = {
   USD: 'United States Dollar',
   AUD: 'Australian Dollar',
@@ -32,3 +37,46 @@ const currencies = {
   ZAR: 'South African Rand',
   EUR: 'Euro',
 };
+
+//populates currencies into html options
+function generateOptions(options){
+  //console.log(options);
+  //looping over an object: Object.entries, Object.values, Object.keys,
+  return Object.entries(options).map(([currencyCode, currencyName]) => {
+    return `<option value="${currencyCode}">
+    ${currencyCode} - ${currencyName}
+    </option>`
+    //console.log(currencyCode, currencyName);
+  }).join(''); //.join turns it into a dump of HTML
+}
+//API function fetch
+async function fetchRates(base = 'USD'){
+  const res = await fetch(`${endpoint}?base=${base}`);
+  const rates = await res.json();
+  return rates;
+ }
+
+ //10:47
+
+const optionsHTML = generateOptions(currencies);
+//console.log(optionsHTML);
+//on page load we can populate the options
+fromSelect.innerHTML = optionsHTML;
+toSelect.innerHTML = optionsHTML;
+
+
+
+//best practice: not going into the UI too much without coding up the actual functionality 
+//if start UI before functionality, tie functionality to tightly to the actual UI
+
+/* const myHeaders = new Headers();
+myHeaders.append("apikey", "yourapikey");
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+fetch(`https://api.apilayer.com/exchangerates_data/latest?symbols={symbols}&base={base}`, requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));  */
