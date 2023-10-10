695963bcc29ea667f845adcaf9db264717331925
Cameron <cameron.delc@gmail.com>
2023-10-10 09:10:42 -0700

    upd

--- exercises/75 - CORS and Recipes/scripts.js
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -2,11 +2,19 @@
 // Please use this replacement API URL "https://recipes.beginnerjavascript.com/api"
 
 const baseEndpoint = 'https://recipes.beginnerjavascript.com/api';
+const proxy = `https://cors-anywhere.herouapp.com/`;
 
-async function fetchRecipes(query){
-    const res = await fetch(`${baseEndpoint}?q=${query}`);
-    const data = await res.json();
+async function fetchRecipes(query) {
+  const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
+  const data = await res.json();
+  console.log(data);
 }
-//4:30
 
 fetchRecipes('pizza');
+
+// CORS
+// CO - cross-origins, domain names are origins, you are not allowed to share data between origins because of a security issue
+
+// The data you are trying to get from another domain has to implement a CORS policy: allows your domain to access data and they will return it, it's safe. Has to happen on the server
+
+// 19:00
